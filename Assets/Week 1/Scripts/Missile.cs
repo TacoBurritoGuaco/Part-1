698ee7683a88437e455075331ec6792304c9500c
Fernando Salas Duran <salasdur@shernet.sheridancollege.ca>
2024-01-18 14:20:36 -0500

    Task-1 complete Missiles collide with the player and are destroyed when they collide Player no longer rotates

--- Assets/Week 1/Scripts/Missile.cs
+++ Assets/Week 1/Scripts/Missile.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
+
     float speed = 12f;
+    Rigidbody2D rigidbody;
+    Vector2 direction = new Vector2(10, 0);
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>(); //Might look familiar from processing!
+        //Its akin to getting a pvector from an arraylist of objects.
+        //In this case, its grabbing the RigidBody2D from the missile and saves it to rigidbody!
+        //Saves a lot of time!
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(speed * Time.deltaTime, 0, 0);
+        //transform.Translate(speed * Time.deltaTime, 0, 0);
+        //Old week-1 code
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + direction * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) //This does not work if you type it by hand
+    {
+        Debug.Log("Missile Hit Something");
+        Destroy(gameObject); //this deletes the game object
     }
 }
