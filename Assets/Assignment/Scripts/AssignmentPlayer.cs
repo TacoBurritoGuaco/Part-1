099c70970b5230380c39ce4f8e6c2f6bed1ec54a
Fernando Salas Duran <salasdur@shernet.sheridancollege.ca>
2024-01-30 17:30:20 -0500

    V2 Completed There's now a distinct end and start screen The end screen appears when a missile hits the player The win screen appears when the player reaches the "Exit" With all of this, all that's left is to do is assembling the game together and polish!

--- Assets/Assignment/Scripts/AssignmentPlayer.cs
+++ Assets/Assignment/Scripts/AssignmentPlayer.cs
@@ -8,9 +8,14 @@ public class AssignmentPlayer : MonoBehaviour
     public Rigidbody2D rigidbody; //The rigidbody
     public float force = 150; //set this to at least 100 or it won't make a difference
     // Start is called before the first frame update
+
+    //The gameover screen, the win screen, and the lose screen as objects used by the player
+    public GameObject gameOverScreen;
+    public GameObject winScreen;
+    public GameObject loseScreen;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -26,5 +31,17 @@ public class AssignmentPlayer : MonoBehaviour
     private void FixedUpdate()
     {
         rigidbody.AddForce(direction * force * Time.deltaTime); //this adds force in the direction that the player is holding
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("PlayerFun")) //if the object called for collision is a missile
+                                                          //This is found using object tags! They are very handy, and I'm glad I found it just by experimenting with object functions
+                                                          //Will keep this in mind for the future.
+        {
+            gameOverScreen.SetActive(true); //turns on the gameoverscreen
+            winScreen.SetActive(false); //turns off exit screen
+            loseScreen.SetActive(true); //turns on the death screen
+        }
     }
 }
--- Assets/Week 1/Scripts/Missile.cs
+++ Assets/Week 1/Scripts/Missile.cs
@@ -9,6 +9,7 @@ public class Missile : MonoBehaviour
     Rigidbody2D rigidbody;
     Vector2 direction = new Vector2(-10, 0); //Direction changed to match to rotation of the sprite and the direction the missile is shot at for assignment 1
     // Start is called before the first frame update
+
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>(); //Might look familiar from processing!
@@ -30,14 +31,17 @@ public class Missile : MonoBehaviour
         rigidbody.MovePosition(rigidbody.position + direction * Time.deltaTime);
     }
 
+    //Important note: "collision" in the case of this function is the object that this object is colliding with
+    //Learnt this from Logan, 2nd year. 
+    //This should allow me to then identify what this object is and as such have different things happen as a result
     private void OnCollisionEnter2D(Collision2D collision) //This does not work if you type it by hand
     {
-        Debug.Log("Missile Hit Something");
+        //Debug.Log("Missile Hit Something");
         Destroy(gameObject); //this deletes the game object
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Missile triggered Something");
+        //Debug.Log("Missile triggered Something");
     }
 }
